6359a1b2e8281dd7e3191fb4dcc52e5e962c7143
Abishek2266-A <abishekmurugesan2006@gmail.com>
2025-11-20 14:47:23 +0530

    first commit

---
+++ topg11.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Arun",
+    "age": 20,
+    "course": "B.Sc Computer Science"
+}
+
+print("Student Name:", student["name"])
+print("Student Age:", student["age"])
+print("Course:", student["course"])
